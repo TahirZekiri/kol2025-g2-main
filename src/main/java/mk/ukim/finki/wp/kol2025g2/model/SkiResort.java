5634b5cb9228c9b4e9727d7a66155fe2c7de502d
TahirZekiri <92637304+TahirZekiri@users.noreply.github.com>
2025-12-17 12:09:24 +0100

    Fix pagination to use 1-based page numbers

--- src/main/java/mk/ukim/finki/wp/kol2025g2/model/SkiResort.java
+++ src/main/java/mk/ukim/finki/wp/kol2025g2/model/SkiResort.java
@@ -1,20 +1,31 @@
 package mk.ukim.finki.wp.kol2025g2.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
+@Entity
 public class SkiResort {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String location;
+
+    @OneToMany(mappedBy = "skiResort")
+    private List<SkiSlope> skiSlopes;
 
     public SkiResort(String name, String location) {
         this.name = name;
         this.location = location;
     }
-
-
 }
--- src/main/java/mk/ukim/finki/wp/kol2025g2/model/exceptions/InvalidSkiSlopeIdException.java
+++ src/main/java/mk/ukim/finki/wp/kol2025g2/model/exceptions/InvalidSkiSlopeIdException.java
@@ -1,4 +1,7 @@
 package mk.ukim.finki.wp.kol2025g2.model.exceptions;
 
 public class InvalidSkiSlopeIdException extends RuntimeException {
+    public InvalidSkiSlopeIdException(Long id) {
+        super(String.format("Ski slope with id %d was not found", id));
+    }
 }
